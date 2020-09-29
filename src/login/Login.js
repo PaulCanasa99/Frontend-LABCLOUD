f880d88e90c5885cdd455a8d6dc090abdad40b19
Paul Canasa <paul.canasa@pucp.edu.pe>
2020-09-29 16:08:31 -0500

    Primer commit

---
+++ src/login/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Login.css';
+const Login = () => {
+  return (
+    <div className="login-background">
+      <div className="login">
+        <img src="../images/logo-pucp.png" className="logo-institucion" />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
